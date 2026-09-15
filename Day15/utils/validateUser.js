16a8901e5ef6407307a099fb1079d12ede4ac6c1
Dilip Kumar <dilip.sharma4558@gmail.com>
2026-09-15 16:15:55 +0530

    Authentication Using Middleware

---
+++ Day15/utils/validateUser.js
@@ -0,0 +1,31 @@
+const validator=require("validator");
+
+//from here 
+ 
+function validateUser(data){
+
+        const mandatoryField=["firstName","emailId","age","password"];
+        const isAllowed=mandatoryField.every((keys)=>Object.keys(data).includes(keys));
+        if(!isAllowed){
+            throw new Error("Fields Missing");
+        }
+
+        if(!validator.isEmail(data.emailId)){
+            throw new Error("Invalid Email");
+        }
+
+        if(!validator.isStrongPassword(data.password)){
+            throw new Error("Weak password");
+        }
+
+        if(!(data.firstName.length>=3 && data.firstName.length<=20)){
+            throw new Error("First Name should have atleast 3 character" );
+        }
+
+
+
+    }
+
+        //to here -> this is API level validation 
+
+module.exports=validateUser;
